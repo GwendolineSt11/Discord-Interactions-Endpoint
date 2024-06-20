8156b326f11fefbb91c4dca3c7c03c9e0445d679
Khristine <kikikhristine@hotmail.com>
2024-06-20 17:19:04 -0600

    Updates

--- discord_interaction/wsgi.py
+++ discord_interaction/wsgi.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from nltk import app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discord_interaction.settings')
 
 application = get_wsgi_application()
+app.run()
