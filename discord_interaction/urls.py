6080558f1d7b9f544707d450241e7a8dbc768c93
Khristine <kikikhristine@hotmail.com>
2024-06-21 13:32:22 -0600

    The same shit

--- discord_interaction/urls.py
+++ discord_interaction/urls.py
@@ -24,7 +24,8 @@ from django.http import HttpResponse
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', lambda request: HttpResponse("Test URL works!")),
-    path('interactions/', views.interactions_view, include('interactions.urls')),
+    path('interactions/', views.interactions_view, name='interactions'),
+    path('interactions/', include('interactions.urls')),
     path('', include('interactions.urls')),
 
 ]
