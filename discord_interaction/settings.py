2d96c79bdc9ffec240afb7c0475c8b35822248a0
Khristine <kikikhristine@hotmail.com>
2024-06-24 13:59:10 -0600

    Authorized new host

--- discord_interaction/settings.py
+++ discord_interaction/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-3$a6x$k02b6077fk@-8t!-x1q+ifcd(s&cd8jz(y3)j=c0=q!^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io', 'discordapp.com', '53c6-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', '4ffc-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'favicon.ico', '6750-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', '93db-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'https://500b-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'https://c45a-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'ngrok-free.app', 'c45a-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'good-man-close.ngrok-free.app', 'discord-interaction-ba1ec86e5d8e.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io', 'discordapp.com', '53c6-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', '4ffc-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'favicon.ico', '6750-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', '93db-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'https://500b-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'https://c45a-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'ngrok-free.app', 'c45a-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'good-man-close.ngrok-free.app', 'discord-interaction-ba1ec86e5d8e.herokuapp.com', 'gwenactivites.online']
 
 # Application definition
 
@@ -68,6 +68,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://good-man-close.ngrok-free.app',
     'https://subdomain.ngrok-free.app',
     'https://c45a-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app',
+    'https://gwenactivites.online',
 ]
 
 ROOT_URLCONF = 'interactions.urls'
