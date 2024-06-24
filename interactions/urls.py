51e7c92a60965461cfa6bed85b1404f0072cb75a
Khristine <kikikhristine@hotmail.com>
2024-06-24 14:30:27 -0600

    fuck my life

--- interactions/urls.py
+++ interactions/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', lambda request: HttpResponse("Test URL works!")),
     path('interactions/', include('interactions.urls')),
-    path('', views.interactions_endpoint, name='interactions'),
+    path('', views.interaction_endpoint, name='interactions'),
 ]
