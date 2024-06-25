d3dd45724af5381fed6582335815acfc659cfc3d
Khristine <kikikhristine@hotmail.com>
2024-06-25 14:50:22 -0600

    what the fuck

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.core.management import execute_from_command_line
 
 def main():
     """Run administrative tasks."""
@@ -19,5 +18,6 @@ def main():
 
 
 if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
     execute_from_command_line(["manage.py", "runserver", "443"])
     main()
