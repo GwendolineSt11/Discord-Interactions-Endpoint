7c878240399b7a447728688598b48bbb24605855
Khristine <kikikhristine@hotmail.com>
2024-06-25 15:22:39 -0600

    .

--- discord_interaction/settings.py
+++ discord_interaction/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '443', '80', '.ngrok.io', 'discordapp
                  'https://c45a-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'ngrok-free.app',
                  'c45a-2806-2f0-4400-4008-b182-bf0b-ef79-aec3.ngrok-free.app', 'good-man-close.ngrok-free.app',
                  'discord-interaction-ba1ec86e5d8e.herokuapp.com', 'gwenactivites.online']
-PORT = 443
+
 # Application definition
 
 INSTALLED_APPS = [
